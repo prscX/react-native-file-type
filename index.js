b23906352801427785fb1231144728472002fd29
Pranav Raj Singh Chauhan <pranav.chauhan@metricstream.com>
2018-09-04 16:56:10 +0530

    - adding support for .html extension

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-import RNFS from 'react-native-fs'
-import { Base64 } from 'js-base64'
-import fileType from 'file-type'
+import RNFS from "react-native-fs";
+import { Base64 } from "js-base64";
+import fileType from "file-type";
 
 function FileType(path) {
   return new Promise((resolve, reject) => {
-    RNFS.readFile(path, 'base64')
+    RNFS.readFile(path, "base64")
       .then(fileData => {
-        let convertedData = CovertBase64ToArrayBuffer(fileData)
-        convertedData = new Uint8Array(convertedData)
+        let convertedData = CovertBase64ToArrayBuffer(fileData);
+        convertedData = new Uint8Array(convertedData);
 
-        let type = fileType(convertedData)
+        let type = fileType(convertedData);
+        if (type === undefined || type === null) {
+          let decodedData = String.fromCharCode.apply(null, convertedData);
 
-        resolve(type)
+          if (
+            decodedData.startsWith("<html>") &&
+            decodedData.endsWith("</html>")
+          ) {
+            type = { ext: "html", mime: "text/html" };
+          }
+        }
+
+        resolve(type);
       })
       .catch(reason => {
-        reject(reason)
-      })
-  })
+        reject(reason);
+      });
+  });
 }
 
 function CovertBase64ToArrayBuffer(data) {
-  let UTF8Data = Base64.atob(data)
-  let UTF8DataLength = UTF8Data.length
+  let UTF8Data = Base64.atob(data);
+  let UTF8DataLength = UTF8Data.length;
 
-  let bytes = new Uint8Array(UTF8DataLength)
+  let bytes = new Uint8Array(UTF8DataLength);
 
   for (var i = 0; i < UTF8DataLength; i++) {
-    bytes[i] = UTF8Data.charCodeAt(i)
+    bytes[i] = UTF8Data.charCodeAt(i);
   }
 
-  return bytes.buffer
+  return bytes.buffer;
 }
 
-export default FileType
+export default FileType;
